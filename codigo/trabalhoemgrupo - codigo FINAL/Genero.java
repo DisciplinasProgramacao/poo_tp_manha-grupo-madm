f489fbbc45f0a038e63df5c02c9367ab3b369700
Maria Luiza Freitas <103125164+MariaLuizaF@users.noreply.github.com>
2023-06-20 22:12:14 -0300

    Add files via upload

---
+++ codigo/trabalhoemgrupo - codigo FINAL/Genero.java
@@ -0,0 +1,14 @@
+package POO.trabalhoemgrupo;
+
+public enum Genero {
+    Ação,
+    Anime,
+    Aventura,
+    Comédia,
+    Documentário,
+    Drama,
+    Policial,
+    Romance,
+    Suspense;
+
+}
